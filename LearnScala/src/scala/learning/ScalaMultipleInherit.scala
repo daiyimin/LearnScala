204a088f82eb18a8bfe9109a18e495897d369157
eyimdai <yimin.dai@ericsson.com>
2016-08-05 11:39:33 +0800

    add multiple inherit

---
+++ LearnScala/src/scala/learning/ScalaMultipleInherit.scala
@@ -0,0 +1,56 @@
+package scala.learning
+
+/** sample 1 **/
+class Human {
+  println("Human")
+}
+
+trait TTeacher extends Human {
+  println("TTeacher")
+  def teach
+}
+
+trait PianoPlayer extends Human {
+  println("PianoPlayer")
+  def playPiano = println("I'm playing piano")
+}
+
+class PianoTeacher extends Human with TTeacher with PianoPlayer {
+ override def teach = println("I am traning students") 
+}
+
+/** sample 2 **/
+trait Action {
+  def doAction
+}
+
+trait BeforeAndAfter extends Action {
+  abstract override def doAction {
+    println("Initializing")
+    super.doAction
+    println("Destroying")
+  }
+}
+
+class Worker extends Action {
+  override def doAction = println("Working...")
+}
+
+object ScalaMultipleInherit {
+  def main(args : Array[String]) {
+     var teacher = new PianoTeacher
+     teacher.teach
+     teacher.playPiano
+     
+     println
+     var human = new Human with TTeacher with PianoPlayer {
+       override def teach = println("Teaching")
+     }
+     human.teach
+     
+     
+     println
+     var worker = new Worker with BeforeAndAfter
+     worker.doAction
+  }
+}
