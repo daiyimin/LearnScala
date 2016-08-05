3774998809ddb6f13459151f82dcce9dc60c641b
eyimdai <yimin.dai@ericsson.com>
2016-08-05 11:04:21 +0800

    add all

---
+++ LearnScala/src/ScalaTrait.scala
@@ -0,0 +1,36 @@
+
+trait Logger {
+  def log (msg : String)
+}
+
+//class ConcreteLogger extends Logger {
+class ConcreteLogger extends Cloneable with Logger {
+  // override is optional when override an abstract method
+  override def log (msg : String) = println ("Log: " + msg)
+  
+  def logging {
+    log("It is me")
+  }
+}
+
+trait LoggerA extends Logger {
+  override def log(msg : String) = println("LogA: " + msg)
+}
+
+trait LoggerB extends Logger {
+  override def log(msg : String) = println("LogB: " + msg)
+}
+
+
+object ScalaTrait {
+  def main(args : Array[String]) {
+     var logger = new ConcreteLogger
+     logger.logging
+     
+     logger = new ConcreteLogger with LoggerA
+     logger.log("hello trait")
+
+     logger = new ConcreteLogger with LoggerB
+     logger.logging  
+  }
+}
